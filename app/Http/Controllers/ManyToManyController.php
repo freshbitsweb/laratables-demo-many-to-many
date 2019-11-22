bb7f2f6d5f13809b0dcb78df0e89d4d2668e8a55
Rajan <rajan@freshbits.in>
2019-11-22 19:23:03 +0530

    display user table data
    Signed-off-by: Rajan <rajan@freshbits.in>

---
+++ app/Http/Controllers/ManyToManyController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Freshbitsweb\Laratables\Laratables;
+
+class ManyToManyController extends Controller
+{
+    /**
+     * Show Table Header column
+     *
+     *
+     * @return Illuminate\Http\Response
+     **/
+    public function index()
+    {
+        return view('manyToManyLaraTable');
+    }
+
+    /**
+     * return data of the Many To Many Relationship datatables.
+     *
+     *
+     * @return Jason
+     **/
+    public function manyToManyData()
+    {
+        return Laratables::recordsOf(User::class);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ManyToManyController@index');
+Route::get('manyToManyData', 'ManyToManyController@manyToManyData')->name('manyToMany');
