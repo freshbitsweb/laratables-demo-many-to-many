d8db839d80e71dde3eca603ce8ed247ef087d0b8
Rajan <rajan@freshbits.in>
2019-11-22 18:51:56 +0530

    users & roles relationship
    Signed-off-by: Rajan <rajan@freshbits.in>

--- app/User.php
+++ app/User.php
@@ -11,4 +11,12 @@ class User extends Authenticatable
     use Notifiable;
 
     public $timestamps = false;
+
+    /**
+     * The roles that belong to the user.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role');
+    }
 }
