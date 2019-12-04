715b2f8bac2bd58c44c6c2f6c98bd82419f05d8b
Rajan <rajan@freshbits.in>
2019-12-04 10:00:11 +0530

    add composite primary key
    Signed-off-by: Rajan <rajan@freshbits.in>

--- database/migrations/2019_11_22_123640_create_role_user_table.php
+++ database/migrations/2019_11_22_123640_create_role_user_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateRoleUserTable extends Migration
 {
@@ -14,8 +14,10 @@ class CreateRoleUserTable extends Migration
     public function up()
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->integer('role_id')->unsigned();
             $table->integer('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+
+            $table->primary(['role_id', 'user_id']);
 
             $table->foreign('role_id')->references('id')->on('roles');
             $table->foreign('user_id')->references('id')->on('users');
